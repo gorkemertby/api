17aa2f5e453f051ba1d3c52263a939ed679d78e0
Gorkem Mert <gorkemert.by@gmail.com>
2025-03-07 11:08:54 +0300

    'UrlsUpdate'

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('blogpost/',views.BlogPostListCreate.as_view(), name = "blogpost-create-view"),
-]
+    path('blogpost/<int:pk>/',views.BlogPostRetrieveUpdateDestroy.as_view() , name = "blogpost-retrieve-update-destroy")
+] 
